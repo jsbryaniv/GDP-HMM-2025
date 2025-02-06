a47b21fc74def171a53f074ef4c77bf957a1e9c1
CarinaMCA <shepperoni.pizza@gmail.com>
2025-02-06 10:21:35 -0700

    made data_downloader a function

--- download_data.py
+++ download_data.py
@@ -7,56 +7,72 @@ import zipfile
 from huggingface_hub import login
 from huggingface_hub import snapshot_download
 
-# Set paths
-path_data = "data"
-path_raw = os.path.join(path_data, "raw_data")
-path_han = os.path.join(path_data, "han")
-path_lung = os.path.join(path_data, "lung")
+
+# Define function to download data
+def download_data(path_data):
+
+    # Set paths
+    path_raw = os.path.join(path_data, "raw_data")
+    path_han = os.path.join(path_data, "han")
+    path_lung = os.path.join(path_data, "lung")
 
 
-### DOWNLOAD DATA ###
-print("Downloading data.")
+    ### DOWNLOAD DATA ###
+    print("Downloading data.")
 
-# Login to the Hugging Face Hub from token in config.json
-try:
-    # Get token from config
+    # Login to the Hugging Face Hub from token in config.json
+    try:
+        # Get token from config
+        with open("config.json", "r") as f:
+            config = json.load(f)
+            token = config["hf_token"]
+        # Login with token
+        login(token)
+        print("Successfully logged into Hugging Face!")
+    except KeyError:  
+        # Exception for when token not in config.json
+        print("Please add your Hugging Face token to config.json")
+    except Exception as e:  
+        # Exception for other errors
+        print(f"Error logging into Hugging Face: {e}")
+
+    # Download the dataset
+    snapshot_download(
+        repo_id="Jungle15/GDP-HMM_Challenge", 
+        repo_type="dataset", 
+        local_dir=path_raw
+    )
+
+
+    ### UNZIP DATA ###
+    print("Unzipping data.")
+
+    # Unzip files
+    paths = [
+        (f"{path_raw}/train_HaN.zip", path_han),
+        (f"{path_raw}/train_Lung.zip", path_lung),
+        (f"{path_raw}/valid_HaN_nodose.zip", path_han),
+        (f"{path_raw}/valid_Lung_nodose.zip", path_lung)
+    ]
+    for (in_path, out_path) in paths:
+        print(f"-- Unzipping {in_path} to {out_path}")
+        with zipfile.ZipFile(in_path, 'r') as zip_ref:
+            zip_ref.extractall(out_path)
+
+    # Done
+    print('Done')
+    return
+
+
+# Download data
+if __name__ == '__main__':
+    
+    # Get data path from config
     with open("config.json", "r") as f:
         config = json.load(f)
-        token = config["hf_token"]
-    # Login with token
-    login(token)
-    print("Successfully logged into Hugging Face!")
-except KeyError:  
-    # Exception for when token not in config.json
-    print("Please add your Hugging Face token to config.json")
-except Exception as e:  
-    # Exception for other errors
-    print(f"Error logging into Hugging Face: {e}")
+        path_data = config["PATH_DATA"]
 
-# Download the dataset
-snapshot_download(
-    repo_id="Jungle15/GDP-HMM_Challenge", 
-    repo_type="dataset", 
-    local_dir=path_raw
-)
+    # Download data
+    download_data(path_data)
 
-
-### UNZIP DATA ###
-print("Unzipping data.")
-
-# Unzip files
-paths = [
-    (f"{path_raw}/train_HaN.zip", path_han),
-    (f"{path_raw}/train_Lung.zip", path_lung),
-    (f"{path_raw}/valid_HaN_nodose.zip", path_han),
-    (f"{path_raw}/valid_Lung_nodose.zip", path_lung)
-]
-for (in_path, out_path) in paths:
-    print(f"-- Unzipping {in_path} to {out_path}")
-    with zipfile.ZipFile(in_path, 'r') as zip_ref:
-        zip_ref.extractall(out_path)
-
-
-# Done
-print('Done')
-
+    
