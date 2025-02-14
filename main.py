70a7207283f204c681990973d4de7da3dd017108
CarinaMCA <shepperoni.pizza@gmail.com>
2025-02-13 18:07:31 -0700

    it was on cpu this whole time

--- main.py
+++ main.py
@@ -259,8 +259,7 @@ def main(
 if __name__ == '__main__':
 
     # Set device
-    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    device = torch.device('cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Set job IDs
     all_jobs = [
