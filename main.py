76767d9cd2920e323ed4f02dd8999461e98de877
CarinaMCA <shepperoni.pizza@gmail.com>
2025-02-28 14:16:06 -0700

    turn degub off

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ if __name__ == '__main__':
     ID = int(sys.argv[1]) if len(sys.argv) > 1 else 0
     ITER = int(sys.argv[2]) if len(sys.argv) > 2 else 0
 
-    # # Debugging
+    # # DEBUGGING
     # for ID in range(len(all_jobs)//2):
     #     for ITER in [0, 1]:
     #         job_args = all_jobs[ID]
