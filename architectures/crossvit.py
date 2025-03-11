Moved scaling into DPModel wrapper. Fixed vit--only use max shape of 64

architectures/crossvit.py
@@ -18,35 +18,11 @@ class CrossViT3d(nn.Module):
     """Cross attention vistion transformer model."""
     def __init__(self,
         in_channels, out_channels, n_cross_channels_list,
-        shape=128, patch_size=16, patch_stride=None,
-        n_features=64, n_heads=4, n_layers=8,
-        n_layers_context=8, n_layers_mixing=8,
+        shape=64, patch_size=8, patch_stride=None,
+        n_features=128, n_heads=4, 
+        n_layers=8, n_layers_context=8, n_layers_mixing=8,
     ):
         super(CrossViT3d, self).__init__()
-
-        # Check inputs
-        if n_layers % 2 != 0:
-            # Number of layers must be even
-            raise ValueError('Number of layers must be even!')
-        if not isinstance(shape, tuple):
-            # Convert shape to tuple
-            shape = (shape, shape, shape)
-        if not isinstance(patch_size, tuple):
-            # Convert patch size to tuple
-            patch_size = (patch_size, patch_size, patch_size)
-        if patch_stride is None:
-            # Set default patch stride (1/2 of patch size)
-            patch_stride = (patch_size[0] // 2, patch_size[1] // 2, patch_size[2] // 2)
-        elif not isinstance(patch_stride, tuple):
-            # Convert patch stride to tuple
-            patch_stride = (patch_stride, patch_stride, patch_stride)
-        for i in range(3):
-            # Check if shape is divisible by patch size
-            if shape[i] % patch_size[i] != 0:
-                raise ValueError('Shape must be divisible by patch size!')
-            # Check if patch size is divisible by patch stride
-            if patch_size[i] % patch_stride[i] != 0:
-                raise ValueError('Patch size must be divisible by patch stride!')
             
         # Set attributes
         self.in_channels = in_channels
@@ -54,30 +30,12 @@ class CrossViT3d(nn.Module):
         self.n_cross_channels_list = n_cross_channels_list
         self.shape = shape
         self.patch_size = patch_size
+        self.patch_stride = patch_stride
         self.n_features = n_features
         self.n_heads = n_heads
         self.n_layers = n_layers
         self.n_layers_context = n_layers_context
         self.n_layers_mixing = n_layers_mixing
-
-        # Get constants
-        shape_patchgrid = (
-            (shape[0] - patch_size[0]) // patch_stride[0] + 1,
-            (shape[1] - patch_size[1]) // patch_stride[1] + 1,
-            (shape[2] - patch_size[2]) // patch_stride[2] + 1,
-        )
-        n_patches = shape_patchgrid[0] * shape_patchgrid[1] * shape_patchgrid[2]
-        n_context = len(n_cross_channels_list)
-        self.patch_stride = patch_stride
-        self.shape_patchgrid = shape_patchgrid
-        self.n_patches = n_patches
-        self.n_context = n_context
-        
-        # Positional and Context Encoding
-        self.pos_embedding = nn.Parameter(.1*torch.randn(1, n_patches, n_features))
-        self.context_embeddings = nn.ParameterList([
-            nn.Parameter(.1*torch.randn(1, 1, n_features)) for _ in range(n_context)
-        ])
 
         # Create main autoencoder
         self.autoencoder = ViT3D(
@@ -100,12 +58,21 @@ class CrossViT3d(nn.Module):
         # Create mixing block
         self.mixing_block = nn.TransformerDecoder(
             nn.TransformerDecoderLayer(
-                d_model=n_features, nhead=n_heads,
+                d_model=n_features, 
+                nhead=n_heads,
                 dim_feedforward=n_features,
                 batch_first=True,
             ),
             num_layers=n_layers_mixing,
         )
+
+        # Create positional and context embeddings
+        n_patches = self.autoencoder.n_patches
+        n_context = len(n_cross_channels_list)
+        self.pos_embedding = nn.Parameter(torch.randn(1, n_patches, n_features))
+        self.context_embeddings = nn.ParameterList([
+            nn.Parameter(torch.randn(1, n_patches, n_features)) for _ in range(n_context)
+        ])
     
     def get_config(self):
         """Get configuration."""
@@ -115,6 +82,7 @@ class CrossViT3d(nn.Module):
             'n_cross_channels_list': self.n_cross_channels_list,
             'shape': self.shape,
             'patch_size': self.patch_size,
+            'patch_stride': self.patch_stride,
             'n_features': self.n_features,
             'n_heads': self.n_heads,
             'n_layers': self.n_layers,
@@ -179,7 +147,12 @@ if __name__ == '__main__':
         n_channels, 1, n_channels_context,
         shape=shape
     )
-    print(f"Number of parameters: {sum(p.numel() for p in model.parameters())}")
+
+    # Print model structure
+    print(f'Model has {sum(p.numel() for p in model.parameters()):,} parameters.')
+    print('Number of parameters in blocks:')
+    for name, block in model.named_children():
+        print(f'--{name}: {sum(p.numel() for p in block.parameters()):,}')
 
     # Create data
     x = torch.randn(1, n_channels, *shape)

architectures/vit.py
@@ -16,8 +16,8 @@ from architectures.blocks import TransformerBlock
 class ViT3D(nn.Module):
     def __init__(self, 
         in_channels, out_channels,
-        shape=(128, 128, 128), patch_size=16, patch_stride=None,
-        n_features=64, n_features_top=8, n_heads=4, n_layers=16,
+        shape=(64, 64, 64), patch_size=8, patch_stride=None,
+        n_features=128, n_heads=4, n_layers=8,
     ):
         super(ViT3D, self).__init__()
 
@@ -51,7 +51,6 @@ class ViT3D(nn.Module):
         self.shape = shape
         self.patch_size = patch_size
         self.n_features = n_features
-        self.n_features_top = n_features_top
         self.n_heads = n_heads
         self.n_layers = n_layers
 
@@ -65,40 +64,43 @@ class ViT3D(nn.Module):
         self.path_stride = patch_stride
         self.shape_patchgrid = shape_patchgrid
         self.n_patches = n_patches
+        # Check it n_patches is too large
+        if n_patches > 10000:
+            raise ValueError('Number of patches is too large! Descrease size or increase patch size and stride.')
 
         # Create input and output blocks
         self.input_block = nn.Sequential(
             # Merge input channels to n_features
-            nn.Conv3d(in_channels, n_features_top, kernel_size=1),
+            nn.Conv3d(in_channels, n_features, kernel_size=1),
         )
         self.output_block = nn.Sequential(
             # Smooth output
             nn.Conv3d(
-                n_features_top, n_features_top, 
+                n_features, n_features, 
                 kernel_size=3, padding=1,
-                groups=n_features_top,
+                groups=n_features,
             ),
             # Project to output channels
-            nn.Conv3d(n_features_top, out_channels, kernel_size=1),
+            nn.Conv3d(n_features, out_channels, kernel_size=1),
         )
         
         # 3D Patch Embedding and Unembedding Layers
         self.patch_embed = nn.Sequential(
             nn.Conv3d(
-                n_features_top, 
+                n_features, 
                 n_features,
                 kernel_size=patch_size, 
                 stride=patch_stride,
-                groups=n_features_top  # Channel-wise patching
+                groups=n_features  # Channel-wise patching
             )
         )
         self.patch_unembed = nn.Sequential(
             nn.ConvTranspose3d(
                 n_features, 
-                n_features_top, 
+                n_features, 
                 kernel_size=patch_size, 
                 stride=patch_stride,
-                groups=n_features_top  # Channel-wise unpatching
+                groups=n_features  # Channel-wise unpatching
             )
         )
         
@@ -155,8 +157,6 @@ class ViT3D(nn.Module):
         x = x + self.pos_embedding.expand(x.shape[0], -1, -1)
 
         # Transformer Encoding
-        # for transformer in self.transformer_encoders:
-        #     x = transformer(x)
         x = self.transformer_encoder(x)
 
         # Return encoded features
@@ -165,8 +165,6 @@ class ViT3D(nn.Module):
     def decoder(self, x):
 
         # Transformer Decoding
-        # for transformer in self.transformer_decoders:
-        #     x = transformer(x)
         x = self.transformer_decoder(x)
 
         # Patch unembedding
@@ -196,7 +194,12 @@ if __name__ == '__main__':
         36, 1, 
         shape=shape, 
     )
+
+    # Print model structure
     print(f'Model has {sum(p.numel() for p in model.parameters()):,} parameters.')
+    print('Number of parameters in blocks:')
+    for name, block in model.named_children():
+        print(f'--{name}: {sum(p.numel() for p in block.parameters()):,}')
 
     # Forward pass
     y = model(x)

model.py
@@ -17,7 +17,8 @@ class DosePredictionModel(nn.Module):
 
         # Check inputs
         if isinstance(shape, int):
-            shape = (shape, shape, shape)
+            # Convert shape to tuple
+            shape = (shape, shape, shape)  
         
         # Set attributes
         self.architecture = architecture
@@ -100,11 +101,31 @@ class DosePredictionModel(nn.Module):
         # Reshape inputs
         transform_params=None  # Initialize to None
         if self.shape is not None:
-            scan, transform_params = resize_image_3d(scan, self.shape, fill_value=scan.min())
+            ### Pad to 256x256x256 ###
+            # Get pad info
+            shape_target = (256, 256, 256)
+            shape_origin = scan.shape[2:]
+            padding = [shape_target[i] - shape_origin[i] for i in range(3)]
+            padding = [(p//2, p-p//2) for p in padding]
+            padding = tuple(sum(padding[::-1], ()))  # Flatten and reverse order
+            # Pad
+            scan = F.pad(scan, padding, value=scan.min())
+            beam = F.pad(beam, padding)
+            ptvs = F.pad(ptvs, padding)
+            oars = F.pad(oars, padding)
+            body = F.pad(body, padding)
+            ### Resize to shape ###
+            scan, resize_params = resize_image_3d(scan, self.shape, fill_value=scan.min())
             beam, _ = resize_image_3d(beam, self.shape)
             ptvs, _ = resize_image_3d(ptvs, self.shape)
             oars, _ = resize_image_3d(oars, self.shape)
             body, _ = resize_image_3d(body, self.shape)
+            ### Update transform params ###
+            transform_params = {
+                'original_shape': shape_origin,
+                'padding': padding,
+                'resize': resize_params,
+            }
 
         # Check architecture
         if self.architecture.lower() in ["unet", "vit"]:
@@ -128,7 +149,19 @@ class DosePredictionModel(nn.Module):
         
         # Reshape prediction
         if self.shape is not None:
-            x = reverse_resize_3d(x, transform_params)
+            ### Extract transform params ###
+            original_shape = transform_params['original_shape']
+            padding = transform_params['padding']
+            resize_params = transform_params['resize']
+            ### Resize to padded shape ###
+            x = reverse_resize_3d(x, resize_params)
+            ### Unpad to original shape ###
+            x = x[
+                :, :, 
+                padding[4]:256-padding[5],
+                padding[2]:256-padding[3],
+                padding[0]:256-padding[1],
+            ]
 
         # Return prediction
         return x
@@ -180,38 +213,38 @@ class DosePredictionModel(nn.Module):
             structures=torch.cat([(ptvs!=0), oars, body], dim=1)
         )
 
-        # Compute reconstruction loss
-        if self.architecture.lower() in ['crossae']:
-            """Compute reconstruction loss for cross-attention-unet."""
-            # Get context
-            y_list = inputs[1]
-            # Get reconstructions
-            recons = self.model.autoencode_context(y_list)
-            # Compute likelihood loss
-            likelihood_recon = (
-                F.mse_loss(y_list[0], recons[0])
-                + F.mse_loss(y_list[1], recons[1])
-                + F.binary_cross_entropy_with_logits(recons[2], y_list[2])  # Use BCE for binary masks
-            )
-            # Combine losses
-            likelihood += likelihood_recon
+        # # Compute reconstruction loss
+        # if self.architecture.lower() in ['crossae']:
+        #     """Compute reconstruction loss for cross-attention-unet."""
+        #     # Get context
+        #     y_list = inputs[1]
+        #     # Get reconstructions
+        #     recons = self.model.autoencode_context(y_list)
+        #     # Compute likelihood loss
+        #     likelihood_recon = (
+        #         F.mse_loss(y_list[0], recons[0])
+        #         + F.mse_loss(y_list[1], recons[1])
+        #         + F.binary_cross_entropy_with_logits(recons[2], y_list[2])  # Use BCE for binary masks
+        #     )
+        #     # Combine losses
+        #     likelihood += likelihood_recon
 
-        elif self.architecture.lower() in ['crossvit']:
-            """Compute reconstruction loss for cross-attention-vit."""
-            # Get context
-            y_list = inputs[1]
-            # Corrupt context
-            y_list_corrupt = [block_mask_3d(y, p=0.5) for y in y_list]
-            # Get reconstructions
-            recons = self.model.autoencode_context(y_list_corrupt)
-            # Compute likelihood loss
-            likelihood_recon = (
-                F.mse_loss(y_list[0], recons[0])
-                + F.mse_loss(y_list[1], recons[1])
-                + F.binary_cross_entropy_with_logits(recons[2], y_list[2])  # Use BCE for binary masks
-            )
-            # Combine losses
-            likelihood += likelihood_recon
+        # elif self.architecture.lower() in ['crossvit']:
+        #     """Compute reconstruction loss for cross-attention-vit."""
+        #     # Get context
+        #     y_list = inputs[1]
+        #     # Corrupt context
+        #     y_list_corrupt = [block_mask_3d(y, p=0.5) for y in y_list]
+        #     # Get reconstructions
+        #     recons = self.model.autoencode_context(y_list_corrupt)
+        #     # Compute likelihood loss
+        #     likelihood_recon = (
+        #         F.mse_loss(y_list[0], recons[0])
+        #         + F.mse_loss(y_list[1], recons[1])
+        #         + F.binary_cross_entropy_with_logits(recons[2], y_list[2])  # Use BCE for binary masks
+        #     )
+        #     # Combine losses
+        #     likelihood += likelihood_recon
 
         # Compute total loss
         loss = (
@@ -249,18 +282,30 @@ class DosePredictionModel(nn.Module):
 # Example usage
 if __name__ == "__main__":
     
-    # Initialize model
-    model = DosePredictionModel(architecture="crossvit", n_channels=5)
-    
     # Initialize inputs
-    scan = torch.randn(1, 1, 32, 32, 32)
-    beam = torch.randn(1, 1, 32, 32, 32)
-    ptvs = torch.randn(1, 1, 32, 32, 32)
-    oars = torch.randn(1, 1, 32, 32, 32)
-    body = torch.randn(1, 1, 32, 32, 32)
+    shape_model = (128, 128, 128)
+    shape_data = (99, 205, 188)
+    scan = torch.randn(1, 1, *shape_data)
+    beam = torch.randn(1, 1, *shape_data)
+    ptvs = torch.randn(1, 1, *shape_data)
+    oars = torch.randn(1, 1, *shape_data)
+    body = torch.randn(1, 1, *shape_data)
+    n_channels = scan.shape[1] + beam.shape[1] + ptvs.shape[1] + oars.shape[1] + body.shape[1]
+    
+    # Initialize model    
+    model = DosePredictionModel(
+        architecture="unet", 
+        shape=shape_model, 
+        n_channels=n_channels, 
+        n_blocks=0,             # Dummy model for testing (0 blocks)
+        n_layers_per_block=0,   # Dummy model for testing (0 layers)
+        n_features=1,           # Dummy model for testing (1 feature)
+        n_groups=1,             # Dummy model for testing (1 group)
+    )
 
     # Forward pass
-    pred = model(scan, beam, ptvs, oars, body)
+    with torch.no_grad():
+        pred = model(scan, beam, ptvs, oars, body)
     
     # Done
     print("Done.")